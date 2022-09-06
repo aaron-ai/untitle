eb20bf220f5314c7b2633a21256aa88cfb79f457
凌楚 <yangkun.ayk@alibaba-inc.com>
2022-09-06 17:18:24 +0800

    WIP

--- main.go
+++ main.go
@@ -2,35 +2,56 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"google.golang.org/grpc/credentials"
 	"log"
-	"os"
+	"strings"
 	v2 "untitle/v2"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	Host       = "47.97.252.220"
-	Port int32 = 8080
+	//Host       = "47.97.252.220"
+	//Port int32 = 8081
+
+	Host       = "127.0.0.1"
+	Port int32 = 9876
 )
 
 func main() {
 	endpoints := fmt.Sprintf("%s:%d", Host, Port)
-	conn, err := grpc.DialContext(context.TODO(), endpoints,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	if err != nil {
-		panic(err)
+	for true {
+		var dopts []grpc.DialOption
+		newTLS := credentials.NewTLS(&tls.Config{
+			RootCAs:            x509.NewCertPool(),
+			InsecureSkipVerify: true,
+		})
+		dopts = append(dopts, grpc.WithTransportCredentials(newTLS))
+		//dopts = append(dopts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+		conn, err := grpc.DialContext(context.TODO(), endpoints, dopts...)
+		if err != nil {
+			panic(err)
+		}
+		conn.Connect()
+		msc := v2.NewMessagingServiceClient(conn)
+		req := &v2.QueryRouteRequest{}
+
+		resp, err := msc.QueryRoute(context.TODO(), req)
+
+		if nil != err {
+			log.Printf("%v, err=%v", resp, err)
+			if !strings.Contains(err.Error(), "empty") && !strings.Contains(err.Error(), "Unknown") {
+				break
+			}
+		} else {
+			log.Print(resp)
+		}
+
+		conn.Close()
+		//os.Exit(1)
 	}
-	conn.Connect()
-	msc := v2.NewMessagingServiceClient(conn)
-	req := &v2.QueryRouteRequest{}
-
-	resp, err := msc.QueryRoute(context.TODO(), req)
-
-	log.Printf("%v, err=%v", resp, err)
-	conn.Close()
-	os.Exit(1)
 }
